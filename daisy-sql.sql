1d9a6f7c53bfdc9f374372606ae4db4706714b24
Cnochur <cnochursaunders@gmail.com>
2025-01-17 13:56:30 +0000

    Added user table

--- daisy-sql.sql
+++ daisy-sql.sql
@@ -4,11 +4,20 @@ CREATE DATABASE IF NOT EXISTS daisy;
 -- Use the database
 USE daisy;
 
+-- Create table 'user' for storing users
+CREATE TABLE IF NOT EXISTS user (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email INT NOT NULL,
+    passwd TEXT,
+);
+
 -- Create table 'entry' for storing mood journal entries
 CREATE TABLE IF NOT EXISTS entry (
     entry_id INT AUTO_INCREMENT PRIMARY KEY,
     emotion VARCHAR(255) NOT NULL,
     tol_score INT NOT NULL,
     reaction TEXT,
-    date_added DATETIME DEFAULT CURRENT_TIMESTAMP
+    date_added DATETIME DEFAULT CURRENT_TIMESTAMP,
+    user_id INT FOREIGN KEY REFERENCES user(user_id)
 );
